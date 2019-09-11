de7ce89de6f67beb092772c2a97028bce4fe9906
boyw165 <boyw165@gmail.com>
2019-09-11 12:06:55 +0800

    Implement the download retry.
    * Use updater dedicated scheduler to coordinate the jobs and states.
    * Move scheduleDelayedCheck() from company method to private method.
    * Fix missing cache clean up.
    * Implement the download retry with 5, 25, 125 seconds repetitive pattern.

--- updaterengine/src/main/java/co/sodalabs/updaterengine/utils/ScheduleUtils.kt
+++ updaterengine/src/main/java/co/sodalabs/updaterengine/utils/ScheduleUtils.kt
@@ -6,6 +6,10 @@ import timber.log.Timber
 
 const val DELAY_ONE_SECOND = 1000L
 
+const val BACKOFF_SECONDS = 5L
+// BACKOFF_SECONDS ^ BACKOFF_RESET_THRESHOLD (given 5^3) is 125 seconds (~2 minutes)
+const val BACKOFF_RESET_THRESHOLD = 3L
+
 object ScheduleUtils {
 
     @Suppress("ReplaceRangeStartEndInclusiveWithFirstLast", "ReplaceJavaStaticMethodWithKotlinAnalog", "ConvertTwoComparisonsToRangeCheck")
@@ -51,5 +55,32 @@ object ScheduleUtils {
             Timber.v("[Updater] It's $currentDateTime and is outside the install window, [$startHour..$endHour], will install the updates at $triggerDateTime, which is $triggerAtMillis milliseconds after")
             triggerAtMillis
         }
+    }
+
+    fun findNextDownloadTimeMillis(
+        attempts: Int
+    ): Long {
+        require(attempts >= 1) { "Invalid attempts $attempts" }
+
+        // We want the power factor as a sequence [1,2,3, 1,2,3, 1,2,3, ...]
+        // e.g.
+        // Input:  1,2,3, 4,5,6, 7,8,9
+        // Output: 1,2,3, 1,2,3, 1,2,3
+        // If the base is 5 seconds, you'll see the backoff as
+        // [5, 25, 125, 5, 25, 125, ...] seconds
+        val throttledAttempts = (attempts - 1) % BACKOFF_RESET_THRESHOLD
+        // The Math.pow requires casting to Double and then cast back to Long.
+        // That's overkilled since we could just do it with a loop.
+        // Time: O(n)
+        var delay = BACKOFF_SECONDS
+        for (i in 0 until throttledAttempts) {
+            delay *= BACKOFF_SECONDS
+        }
+        val currentDateTime = Instant.now()
+            .atZone(ZoneId.systemDefault())
+        Timber.v("[Updater] Given $attempts attempts, retry after $delay seconds")
+        val retryDateTime = currentDateTime.plusSeconds(delay)
+        Timber.v("[Updater] It's $currentDateTime and will retry downloading at $retryDateTime")
+        return retryDateTime.toInstant().toEpochMilli()
     }
 }
